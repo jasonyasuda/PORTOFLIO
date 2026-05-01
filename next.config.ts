75faca7b1d0e8e3102eebdd7d0029d796af90d38
jasonyasuda <jyasuda@ucsd.edu>
2026-05-01 00:20:25 -0700

    Configured GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: isProd ? '/my-app/' : '',
+  basePath: isProd ? '/my-app' : '',
+  output: 'export',
 };
 
 export default nextConfig;
