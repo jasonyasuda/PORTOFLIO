0c616c0960c2ac2773d4d55913bddcc456eb9102
jasonyasuda <jyasuda@ucsd.edu>
2026-05-01 00:26:36 -0700

    Updates

--- _next/static/f5ls3SzfCR6dRgiSwWS7N/_buildManifest.js
+++ _next/static/f5ls3SzfCR6dRgiSwWS7N/_buildManifest.js
@@ -3,8 +3,8 @@ self.__BUILD_MANIFEST = {
     "afterFiles": [],
     "beforeFiles": [
       {
-        "source": "/my-app//_next/:path+",
-        "destination": "/my-app/_next/:path+"
+        "source": "/PORTOFLIO//_next/:path+",
+        "destination": "/PORTOFLIO/_next/:path+"
       }
     ],
     "fallback": []
